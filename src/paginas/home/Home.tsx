8c1ffaaad163d0e3a1d04fae7a632dcfd3661b33
felipe <felipe_blade@hotmail.com>
2022-07-26 21:03:17 -0300

    Atividade React 1

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import'./Home.css';
+
+function Home(){
+    return (
+        <div>
+            <h1 className="titulo">Home</h1>
+            <img src="https://miro.medium.com/max/1400/1*TMYPt-WhRNKUTdXXQ0N2SQ.jpeg" alt="Imagem Tela Inicial" className='img' /> 
+        </div>
+    );
+}
+
+export default Home;
